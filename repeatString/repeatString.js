b21b9014f8e6511b00ff91e59b102eab11ecc86a
Simon Sardorf <ssardorf@gmail.com>
2019-11-27 21:15:19 +0100

    Finished exercises helloWorld, repeatString, reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, timesRepeated) {
+    let finalString = ""
+    if (timesRepeated < 0){
+        return "ERROR"
+    }
+    for(i=0; i<timesRepeated; i++){
+        finalString += text
+    }
+    return finalString
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textSplit = text.split("")
+    let reversedString = []
+    for(i=textSplit.length-1;i>=0;i--){
+        reversedString.push(textSplit[i])
+    }
+    reversedString = reversedString.join("")
+    return reversedString
+    
 }
-
 module.exports = reverseString
